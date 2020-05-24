34f4eff5b6a796c2bd84262a69d31f62e85dc1a7
Liadrinz <435438602@qq.com>
2020-05-24 10:01:12 +0800

    a seemly successful version

--- quill-backend/src/backend/ot.js
+++ quill-backend/src/backend/ot.js
@@ -4,14 +4,7 @@ function ot(history, delta, oldDelta) {
     console.log('before', history)
     history = new Delta().insert('\n').compose(history);
     console.log('later', history);
-    let diff = history.diff(oldDelta);
-    // let i = 1;
-    // let lastEnterInsert = diff.ops[diff.ops.length - i];
-    // while (!lastEnterInsert.insert || lastEnterInsert.insert.indexOf('\n') == -1) {
-    //     lastEnterInsert = diff.ops[diff.ops.length - ++i];
-    // }
-    // lastEnterInsert.insert = lastEnterInsert.insert.substr(1);
-    // if (lastEnterInsert.insert == '') diff.ops.splice(-i, 1);
+    let diff = oldDelta.diff(history);
     if (diff.ops.length > 0)
         return diff.transform(delta, true);
     return delta;
