958194193aefc264931da96f9742788f0296f04f
ahuang172 <842018761@qq.com>
2020-05-27 22:53:32 +0800

    tt

--- MonkeyDocsStruts/src/main/java/com/monkey/mvc/VisitdocsController.java
+++ MonkeyDocsStruts/src/main/java/com/monkey/mvc/VisitdocsController.java
@@ -1,5 +1,6 @@
 package com.monkey.mvc;
 
+import com.google.gson.JsonObject;
 import com.monkey.dao.DocvisittokenDAO;
 import com.monkey.dao.MetaDAO;
 import com.monkey.dao.MetaToUserDAO;
@@ -7,13 +8,14 @@ import com.monkey.entity.Docvisittoken;
 import com.monkey.entity.MetaToUser;
 import com.monkey.util.Security;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/visitdocs")
@@ -49,6 +51,42 @@ public class VisitdocsController {
         redirectView.setContextRelative(true);
         redirectView.setUrl("/DocEditPage.html?docid="+docid+"&action=join");
         return redirectView;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/jugvisittoken",method = RequestMethod.POST)
+    public Boolean jugvisittoken(@RequestBody Vistittokenpost vistittokenpost){
+        Boolean ifhashtoken =false;
+        List<Docvisittoken> docvisittokens = new ArrayList<Docvisittoken>();
+        List<Docvisittoken> list = docvisittokenDAO.findAll();
+        for(Docvisittoken docvisittoken:list){
+            if(docvisittoken.getMdId() == vistittokenpost.mdId){
+                    if(docvisittoken.getToken().equals(vistittokenpost.visittoken)){
+                        ifhashtoken =true;
+                    }
+            }
+        }
+        return ifhashtoken;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/joindoc",method = RequestMethod.POST)
+    public MetaToUser joindoc(@RequestBody JoinParam joinParam){
+        MetaToUser metaToUser = new MetaToUser();
+        metaToUser.setMdId(joinParam.mdId);
+        metaToUser.setUserId(joinParam.userId);
+        metaToUser.setRole("collaborator");
+        MetaToUser metaToUser1;
+        metaToUser1 =  metaToUserDAO.create(metaToUser);
+        return metaToUser1;
+    }
+    public static class JoinParam {
+        public Integer mdId;
+        public Integer userId;
+    }
+    public static class Vistittokenpost {
+        public Integer mdId;
+        public String visittoken;
     }
 
 }
